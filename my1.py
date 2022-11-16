2f28cbb2cd3c21952415af1632a273af67adc1dc
ZeiN132 <82226411+ZeiN132@users.noreply.github.com>
2022-11-17 01:42:15 +0300

    Add files via upload

---
+++ my1.py
@@ -0,0 +1,41 @@
+from aiogram import types, executor, Dispatcher, Bot
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+
+bot = Bot(token="5459967628:AAETxhsqUv7xJ6QzY_B5Iq8hcditzQ_dy_A")
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def begin(message: types.Message):
+    await bot.send_message(message.chat.id, "Привет")
+
+
+@dp.message_handler(content_types=['text'])
+async def text(message: types.Message):
+    url = "https://ru.wikipedia.org/w/index.php?go=Перейти&search=" + message.text
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, "html.parser")
+
+    links = soup.find_all("div", class_="mw-search-result-heading")
+
+    if len(links) > 0:
+        url = "https://ru.wikipedia.org" + links[0].find("a")["href"]
+
+    option = webdriver.ChromeOptions()
+    option.add_argument('headless')
+
+    driver = webdriver.Chrome(chrome_options=option)
+    driver.get(url)
+
+    driver.execute_script("window.scrollTo(0, 180)")
+    driver.save_screenshot("img.png")
+    driver.close()
+
+    photo = open("img.png", 'rb')
+    await bot.send_photo(message.chat.id, photo=photo, caption=f'Ссылка на статью: <a href="{url}">клик</a>',
+                         parse_mode="HTML")
+
+
+executor.start_polling(dp)
